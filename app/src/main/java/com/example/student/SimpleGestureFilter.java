b3eb3497d719fe82aa7b95c5ca77d4cf22a567e3
viral9 <58467817+viral9@users.noreply.github.com>
2020-02-05 16:50:27 +0530

    add gesture

--- app/src/main/java/com/example/student/SimpleGestureFilter.java
+++ app/src/main/java/com/example/student/SimpleGestureFilter.java
@@ -2,6 +2,7 @@ package com.example.student;
 
 import android.app.Activity;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class SimpleGestureFilter extends GestureDetector.SimpleOnGestureListener {
     public final static int SWIPE_UP = 0;
@@ -17,12 +18,161 @@ public class SimpleGestureFilter extends GestureDetector.SimpleOnGestureListener
     private int swipe_Min_Distance = 100;
     private int swipe_Max_Distance = 350;
     private int swipe_Min_Velocity = 100;
-
     private int MODE = MODE_DYNAMIC;
     private boolean running = true;
     private boolean tapIndicator = false;
 
     private Activity context;
     private GestureDetector detector;
-    //private
+    //SimpleGestureFilter simpleGestureFilter;
+    //private GestureDetector.SimpleOnGestureListener listener;
+    private SimpleGestureListener listener;
+    public SimpleGestureFilter(Activity context, SimpleGestureListener sgl)
+    {
+        this.context = context;
+        this.detector = new GestureDetector(context,this);
+        this.listener = (SimpleGestureListener) sgl;
+
+    }
+    public void onTouchEvent(MotionEvent event)
+    {
+        if(!this.running)
+        {
+            return;
+        }
+        boolean result = this.detector.onTouchEvent(event);
+        if(this.MODE == MODE_SOLID)
+        {
+            event.setAction(MotionEvent.ACTION_CANCEL);
+        }
+        else if(this.MODE == MODE_DYNAMIC)
+        {
+            if(event.getAction() == ACTION_FAKE)
+            {
+                event.setAction(MotionEvent.ACTION_UP);
+            }
+            else if(result)
+            {
+                event.setAction(MotionEvent.ACTION_CANCEL);
+            }
+            else if(this.tapIndicator)
+            {
+                event.setAction(MotionEvent.ACTION_DOWN);
+                this.tapIndicator = false;
+            }
+        }
+    }
+
+    public int getSwipe_Min_Distance() {
+        return swipe_Min_Distance;
+    }
+
+    public void setSwipe_Min_Distance(int swipe_Min_Distance) {
+        this.swipe_Min_Distance = swipe_Min_Distance;
+    }
+
+    public int getSwipe_Max_Distance() {
+        return swipe_Max_Distance;
+    }
+
+    public void setSwipe_Max_Distance(int swipe_Max_Distance) {
+        this.swipe_Max_Distance = swipe_Max_Distance;
+    }
+
+    public int getSwipe_Min_Velocity() {
+        return swipe_Min_Velocity;
+    }
+
+    public void setSwipe_Min_Velocity(int swipe_Min_Velocity) {
+        this.swipe_Min_Velocity = swipe_Min_Velocity;
+    }
+
+    public int getMODE() {
+        return MODE;
+    }
+
+    public void setMODE(int MODE) {
+        this.MODE = MODE;
+    }
+
+    public void setEnabled(boolean status)
+    {
+        this.running = status;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+
+        final float xDistance = Math.abs(e1.getX()- e2.getX());
+        final float yDistance = Math.abs(e1.getY() - e2.getY());
+
+        if(xDistance > this.swipe_Max_Distance || yDistance > this.swipe_Max_Distance)
+        {
+            return false;
+        }
+        velocityX = Math.abs(velocityX);
+        velocityY = Math.abs(velocityY);
+        boolean result = false;
+        if(velocityX > this.swipe_Min_Velocity && xDistance > this.swipe_Min_Distance)
+        {
+            if(e1.getX() > e2.getX())
+            {
+                this.listener.onSwipe(SWIPE_LEFT);
+            }
+            else
+            {
+                this.listener.onSwipe(SWIPE_RIGHT);
+            }
+            result = true;
+        }
+        else if(velocityY > this.swipe_Min_Velocity && yDistance > this.swipe_Min_Distance)
+        {
+            if(e1.getY() > e2.getY())
+            {
+                this.listener.onSwipe(SWIPE_UP);
+            }
+            else
+            {
+                this.listener.onSwipe(SWIPE_DOWN);
+            }
+            result = true;
+        }
+        return result;
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        this.tapIndicator = true;
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        this.listener.onDoubleTap();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        if(this.MODE == MODE_DYNAMIC)
+        {
+            e.setAction(ACTION_FAKE);
+            this.context.dispatchTouchEvent(e);
+        }
+        return false;
+    }
+
+    static interface SimpleGestureListener
+    {
+        void onSwipe(int direction);
+        void onDoubleTap();
+
+    }
 }
--- app/src/main/java/com/example/student/up_seek.java
+++ app/src/main/java/com/example/student/up_seek.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.pwittchen.swipe.library.rx2.SimpleSwipeListener;
+import com.github.pwittchen.swipe.library.rx2.Swipe;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-public class up_seek extends AppCompatActivity {
+public class up_seek extends AppCompatActivity implements SimpleGestureFilter.SimpleGestureListener {
+
+    //private SimpleGestureFilter detector;
 
     BottomSheetBehavior bottomSheetBehavior;
     TextView tvstatsus;
     Button btnexpand,btncollapse;
+
+    Swipe swipe;
 
 
     @Override
@@ -23,6 +31,34 @@ public class up_seek extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_up_seek);
         View bottomsheet = findViewById(R.id.bottom_sheet);
+        //detector = new SimpleGestureFilter(this,this);
+
+        swipe = new Swipe();
+        swipe.setListener(new SimpleSwipeListener() {
+            @Override
+            public void onSwipingLeft(MotionEvent event) {
+                Toast.makeText(up_seek.this, "swipe left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipingRight(MotionEvent event) {
+                Toast.makeText(up_seek.this, "swipe right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipingUp(MotionEvent event) {
+                Toast.makeText(up_seek.this, "swipe up", Toast.LENGTH_SHORT).show();
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+
+            @Override
+            public void onSwipingDown(MotionEvent event) {
+                Toast.makeText(up_seek.this, "swipe down", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomsheet);
         tvstatsus = findViewById(R.id.tvstatus);
@@ -73,5 +109,41 @@ public class up_seek extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+       // this.detector.onTouchEvent(ev);
+        swipe.dispatchTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onSwipe(int direction) {
+        String str = "";
+        switch (direction)
+        {
+            case SimpleGestureFilter.SWIPE_RIGHT:
+                str = "swipe_right";
+                break;
+            case SimpleGestureFilter.SWIPE_LEFT:
+                str = "swipe_left";
+                break;
+            case SimpleGestureFilter.SWIPE_UP:
+                str = "swipe_up";
+                break;
+            case SimpleGestureFilter.SWIPE_DOWN:
+                str = "swipe_down";
+                break;
+        }
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onDoubleTap() {
+        Toast.makeText(this, "Double tap", Toast.LENGTH_SHORT).show();
+
     }
 }
